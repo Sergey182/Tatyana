1f1068023874001ad4152ab3f88393f6d2a1f591
Sergey <sinitsyn2012@mail.ru>
2021-02-01 16:06:00 +0300

    ПК версия

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.burger-btn').on('click', function(e) {
+        e.preventDefault();
+        $('.burger-btn').toggleClass('burger-btn--active');
+        $('.header__nav').toggleClass('header__nav--active');
+        
+    });
+    
+
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+
+    $('.intro__sliders').slick({
+        autoplay:true,
+        dots:true,
+        infinite:true,
+        pauseOnHover:false,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+);
+});
+
+
+
+
