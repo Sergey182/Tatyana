9e07ead9b66dae44ac87fff55541efe6fb3c411c
Semyon Panow <saimonpanov@gmail.com>
2021-02-12 12:32:33 +0300

    реализованы: модальная форма, валидация и маски ввода данных в формы ввода, отправка данных из форм ввода на почту, произведен небольшой рефакторинг кода

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-    $email = "saimonpanov@gmail.com"
+    $email = "saimonpanov@gmail.com";
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
     $name = trim($name);
     $phone = trim($phone);
     $message = trim($message);
+    $content .= "Имя: ".$name."\nНомер телефона: ".$phone."\nСообщение: ".$message;
 
-    if (mail($email, "Заявка с сайта", "Имя: ".$name.". Номер телефона: ".$phone". Сообщение: ".$message)) {
+    if (mail($email, "Заявка с сайта", $content)) {
         echo "сообщение успешно отправлено";
     } else {
         echo "при отправке сообщения возникли ошибки";
